6b9e843aa7e31a41f207470c7776e0865343a797
Shajib96 <shajibahamedornob@gmail.com>
2025-05-09 23:36:28 +0600

    Learn about datatype

---
+++ DataType.js
@@ -0,0 +1,30 @@
+const name = "Shajib";
+
+let age = 34;
+
+let isGraduation = false;
+
+// Data type 
+
+/*
+   1. Number
+   2. String
+   3. Boolean
+   4. null
+   5. undefined
+   6.symbol =>  when we need to unique data
+
+
+   7. Object
+
+   when to need to dataType;
+   use typeof;
+*/
+
+console.log(typeof("hello"));
+console.log(typeof(77));
+console.log(typeof(undefiend));
+console.log(typeof(null));  
+// Null is a object .
+
+
