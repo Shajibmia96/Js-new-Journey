b76c69b5c2a51880fc48b671956a3e2884c16a0e
Shajib96 <shajibahamedornob@gmail.com>
2025-05-09 23:04:57 +0600

    Var added

--- Test.js
+++ Test.js
@@ -1,1 +1,13 @@
 console.log("Hello, This is Shajib");
+
+const userId = 8554;
+let userName = "Shajib Mia";
+var userCity = "Narayangonj";
+userState = "Araihazar";
+console.table([userName, userId, userCity]);
+
+
+/* 
+  prefer not use var because of issue in block scope and functional scope;
+*/
+
